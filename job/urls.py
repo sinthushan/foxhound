233df89a6d7d12b490c103869c93355b38e76856
sinthushan <sinthushan@gmail.com>
2024-08-19 19:06:33 -0400

    added view to create a stage in application path

--- job/urls.py
+++ job/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import JobList, JobDetail
+from .views import JobList, JobDetail, StageCreate
 
 
 urlpatterns = [
-    path("<int:pk>/", JobDetail.as_view(), name="job_detail"),
-    path("", JobList.as_view(), name="job_list")
+    path("jobs/<int:pk>/", JobDetail.as_view(), name="job_detail"),
+    path("jobs/", JobList.as_view(), name="job_list"),
+    path("stages/", StageCreate.as_view(), name="stage_create")
 ]
--- job/views.py
+++ job/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from .models import Job
-from .serializers import JobSerializer
+from .models import Job, Stage
+from .serializers import JobSerializer, StageSeralizer
 
 class JobList(generics.ListCreateAPIView):
     queryset = Job.objects.all()
@@ -9,3 +9,7 @@ class JobList(generics.ListCreateAPIView):
 class JobDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
+
+class StageCreate(generics.CreateAPIView):
+    queryset = Stage.objects.all()
+    serializer_class = StageSeralizer
