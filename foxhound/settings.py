7eca8830eb7bcdc613154a887753fb7105b63073
sinthushan <sinthushan@gmail.com>
2024-08-22 19:53:53 -0400

    modifying default auth to test frontend

--- foxhound/settings.py
+++ foxhound/settings.py
@@ -65,11 +65,11 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
+    'http://localhost:4200',
     'http://localhost:8000',
 )
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"] 
+CSRF_TRUSTED_ORIGINS = ["http://localhost:4200"] 
 
 ROOT_URLCONF = 'foxhound.urls'
 
@@ -151,7 +151,7 @@ AUTH_USER_MODEL  = "user.Applicant"
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES":[
-        "rest_framework.permissions.IsAuthenticated"
+        "rest_framework.permissions.AllowAny"
     ],
     "DEFAULT_AUTHENTICATION_CLASSES":[
         "rest_framework.authentication.SessionAuthentication",
